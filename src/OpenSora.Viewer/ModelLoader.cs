7b03085aa5937b11f3911b1a6f2d2395b7e924be
Roman Shapiro <eleroman@mail.ru>
2019-09-22 03:36:36 +0700

    Finished basic model decompression

--- src/OpenSora.Viewer/ModelLoader.cs
+++ src/OpenSora.Viewer/ModelLoader.cs
@@ -5,30 +5,46 @@ namespace OpenSora.Viewer
 {
 	static class ModelLoader
 	{
-		private static byte[] Decompress(BinaryReader reader)
+		private class Decompressor
 		{
-			var output = new byte[65535];
-			byte bits = 8; //8 to start off with, then 16
-			var flags = reader.ReadUInt16();
-			flags >>= 8;
-			ushort outputoffset = 0;
+			byte _bits = 9; //8 to start off with, then 16
+			ushort _flags;
+			BinaryReader _reader;
+			private readonly byte[] _output = new byte[65536];
+			int _outputOffset;
 
-			// Setup / reset generator
-			Func<bool> getflag = () =>
+			public byte[] Output
 			{
-				if (bits == 0)
+				get
 				{
-					flags = reader.ReadUInt16();
-					bits = 16;
+					return _output;
+				}
+			}
+
+			public int OutputSize
+			{
+				get
+				{
+					return _outputOffset;
+				}
+			}
+
+
+			private bool getflag()
+			{
+				if (_bits == 0)
+				{
+					_flags = _reader.ReadUInt16();
+					_bits = 16;
 				}
 
-				var flag = flags & 1;
-				flags >>= 1;
-				bits -= 1;
+				var flag = _flags & 1;
+				_flags >>= 1;
+				_bits -= 1;
 				return flag != 0;
-			};
+			}
 
-			Action<int> setup_run = (int prev_u_buffer_pos) =>
+			void setup_run(int prev_u_buffer_pos)
 			{
 				ushort run = 2;
 				if (!getflag())
@@ -44,7 +60,7 @@ namespace OpenSora.Viewer
 							{
 								if (!getflag())
 								{
-									run = reader.ReadByte();
+									run = _reader.ReadByte();
 									run += 0xE;
 								}
 								else
@@ -60,97 +76,129 @@ namespace OpenSora.Viewer
 						}
 					}
 				}
+
 				// Does the 'copy from buffer' thing
 				for (var i = 1; i <= run; ++i)
 				{
-					output[outputoffset] = output[outputoffset - prev_u_buffer_pos];
-					outputoffset += 1;
+					_output[_outputOffset] = _output[_outputOffset - prev_u_buffer_pos];
+					_outputOffset += 1;
 				}
-			};
+			}
 
-			while (true)
+			public void Decompress(BinaryReader r)
 			{
-				if (getflag())
-				{
-					//Call next method to process next flag
-					var prev_u_buffer_pos = 0;
-					if (getflag())  // Long look-back distance or exit program or repeating sequence(flags = 11)
-					{
-						ushort run = 0;
-						for (var i = 1; i <= 5; ++i) // Load high - order distance from flags (max = 0x31)
-						{
-							run = (ushort)((run << 1) | (getflag() ? 1 : 0));
-						}
+				_reader = r;
+				_bits = 8; //8 to start off with, then 16
+				_flags = _reader.ReadUInt16();
 
-						prev_u_buffer_pos = reader.ReadByte();// Load low - order distance(max = 0xFF)
-															  // Also acts as flag byte
-															  // run = 0 and byte = 0->exit program
-															  // run = 0 and byte = 1->sequence of repeating bytes
-						if (run != 0)
+				_flags >>= 8;
+				_outputOffset = 0;
+
+				while (true)
+				{
+					if (getflag())
+					{
+						// Call next method to process next flag
+						if (getflag()) // Long look-back distance or exit program or repeating sequence(flags = 11)
 						{
-							prev_u_buffer_pos = prev_u_buffer_pos | (run << 8);// Add high and low order distance(max distance = 0x31FF)
-							setup_run(prev_u_buffer_pos);// Get run length and finish unpacking(write to output)
-						}
-						else if (prev_u_buffer_pos > 2)// Is this used? Seems inefficient.
-						{
-							setup_run(prev_u_buffer_pos);
-						}
-						else if (prev_u_buffer_pos == 0)// Decompression complete. End program.
-						{
-							break;
-						}
-						else // Repeating byte
-						{
-							var branch = getflag();// True = long repeating sequence(> 30)
-							for (var i = 1; i <= 4; ++i)
-							{// Load run length from flags
+							ushort run = 0;
+							for (var i = 1; i <= 5; ++i) // Load high - order distance from flags (max = 0x31)
+							{
 								run = (ushort)((run << 1) | (getflag() ? 1 : 0));
 							}
-							if (branch)
-							{// Long run length
-								run = (ushort)((run << 0x8) | reader.ReadByte());// Load run length from byte and add high-order run length(max = 0xFFF + 0xE)
-							}
-							run += 0xE;
-							var b = reader.ReadByte();
-							for (var i = 0; i < run; ++i)
+
+							int prev_u_buffer_pos = _reader.ReadByte();// Load low - order distance(max = 0xFF)
+																	  // Also acts as flag byte
+																	  // run = 0 and byte = 0->exit program
+																	  // run = 0 and byte = 1->sequence of repeating bytes
+							if (run != 0)
 							{
-								output[outputoffset++] = b;
+								prev_u_buffer_pos = prev_u_buffer_pos | (run << 8); // Add high and low order distance(max distance = 0x31FF)
+								setup_run(prev_u_buffer_pos); // Get run length and finish unpacking(write to output)
 							}
+							else if (prev_u_buffer_pos > 2) // Is this used? Seems inefficient.
+							{
+								setup_run(prev_u_buffer_pos);
+							}
+							else if (prev_u_buffer_pos == 0) // Decompression complete. End program.
+							{
+								break;
+							}
+							else // Repeating byte
+							{
+								var branch = getflag(); // True = long repeating sequence(> 30)
+								for (var i = 1; i <= 4; ++i)
+								{
+									// Load run length from flags
+									run = (ushort)((run << 1) | (getflag() ? 1 : 0));
+								}
+								if (branch)
+								{
+									// Long run length
+									run = (ushort)((run << 0x8) | _reader.ReadByte());// Load run length from byte and add high-order run length(max = 0xFFF + 0xE)
+								}
+								run += 0xE;
+								var b = _reader.ReadByte();
+								for (var i = 0; i < run; ++i)
+								{
+									_output[_outputOffset++] = b;
+								}
+							}
+						}
+						else
+						{
+							// Short look - back distance(flags = 10)
+							var prev_u_buffer_pos = _reader.ReadByte();// Get the look - back distance(max = 0xFF)
+							setup_run(prev_u_buffer_pos);// Get run length and finish unpacking(write to output)
 						}
 					}
 					else
-					{// Short look - back distance(flags = 10)
-						prev_u_buffer_pos = reader.ReadByte();// Get the look - back distance(max = 0xFF)
-						setup_run(prev_u_buffer_pos);// Get run length and finish unpacking(write to output)
-
+					{
+						// Copy byte(flags = 0)
+						_output[_outputOffset] = _reader.ReadByte();
+						_outputOffset += 1;
 					}
 				}
-				else
-				{ // Copy byte(flags = 0)
-					output[outputoffset] = reader.ReadByte();
-					outputoffset += 1;
-				}
 			}
-			return output;
-
 		}
 
 		public static void LoadModel(byte[] data)
 		{
-			using (var stream = new MemoryStream(data))
-			using(var reader = new BinaryReader(stream))
+			byte[] result;
+			using (var output = new MemoryStream())
 			{
-				var size = reader.ReadUInt16();
+				var decompressor = new Decompressor();
+				using (var stream = new MemoryStream(data))
+				using (var reader = new BinaryReader(stream))
+				{
+					while (true)
+					{
+						var size = reader.ReadUInt16();
+						var method = reader.ReadByte();
+						stream.Seek(-1, SeekOrigin.Current);
+						if (method == 0)
+						{
+							decompressor.Decompress(reader);
 
-				var method = reader.ReadByte();
-				if (method == 0)
-				{
-					data = Decompress(reader);
-				} else
-				{
-					throw new NotImplementedException();
+							output.Write(decompressor.Output, 0, decompressor.OutputSize);
+						}
+						else
+						{
+							throw new NotImplementedException();
+						}
+
+						var flag = reader.ReadByte();
+						if (flag == 0)
+						{
+							break;
+						}
+					}
 				}
+
+				result = output.ToArray();
 			}
+
+			File.WriteAllBytes("d:\\temp\\output.x2", result);
 		}
 	}
 }
